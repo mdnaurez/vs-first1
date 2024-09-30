58b3f207392a2689d10de3d4d9220b23ffebb5fd
naurez <mdnaurezalam888@gmail.com>
2024-09-30 18:12:46 +0530

    sum()

---
+++ first.java
@@ -0,0 +1,31 @@
+
+
+
+
+public class caalc {
+	public int sum(int a, int b) {
+		int c;
+		c=5+10;
+		return c;
+	}
+	public int diff(int a, int b) {
+		return a-b;
+	}
+	public int mul(int a, int b) {
+		return a*b;
+	}
+	public int div(int a, int b) {
+		return a/b;
+	}
+	public void show() {
+		System.out.println("hii i am naurez");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    caalc c=new caalc();
+		System.out.println("sum is"+c.sum(20, 10));
+		System.out.println("sum is"+c.sum(20, 10));
+	}
+
+}
